89827ea0fd9ad66295f47c3789d61a1bc29d8311
Alexander Henry <10469240+afmhenry@users.noreply.github.com>
2021-12-01 21:53:52 +0100

    re-structured and beat second task of dec 1

--- dec1/main.py
+++ dec1/main.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
-import sys
-import re
-
 import csv
+
+
+def count3PointDeeperReadings(filename):
+    with open(filename, encoding="latin1") as f:
+        counter = 0
+        value_1 = 0
+        value_2 = 0
+        value_3 = 0
+        curr_sum = 0
+        prev_sum = 0
+        for index, row in enumerate(csv.reader(f, delimiter=';')):
+            current_value = int(row[0])
+            value_3 = value_2
+            value_2 = value_1
+            value_1 = current_value
+            if value_1 != 0 and value_2 != 0 and value_3 != 0:
+                curr_sum = value_1 + value_2 + value_3
+                if prev_sum != 0 and prev_sum < curr_sum:
+                    counter += 1
+                prev_sum = curr_sum
+
+    return counter
 
 
 def countDeeperReadings(filename):
@@ -18,4 +37,8 @@ def countDeeperReadings(filename):
 
 
 assert (countDeeperReadings("dec1-mock.txt") == 7)
-print( countDeeperReadings("dec1.txt") )
+print(countDeeperReadings("dec1.txt"))
+
+
+assert(count3PointDeeperReadings("dec1-2.txt") == 5)
+print(count3PointDeeperReadings("dec1.txt"))
