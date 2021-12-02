2f6b1f1378fda99aa35a880ecc38cbf2e6c7734f
Alexander Henry <10469240+afmhenry@users.noreply.github.com>
2021-12-02 19:37:21 +0100

    dec2 challenge 2 done, ezpz

--- dec2/main.py
+++ dec2/main.py
@@ -19,5 +19,28 @@ def countPosition(filename):
     return distance * depth
 
 
+def countPositionWithAim(filename):
+    with open(filename, encoding="latin1") as f:
+        distance = 0
+        depth = 0
+        aim = 0
+
+        for index, row in enumerate(csv.reader(f, delimiter=' ')):
+            operation = row[0]
+            value = int(row[1])
+            if operation == "forward":
+                distance += value
+                depth += aim * value
+            elif operation == "down":
+                aim += value
+            else:
+                aim -= value
+
+    return distance * depth
+
+
 assert(countPosition("dec2-mock.txt") == 150)
 print(countPosition("dec2.txt"))
+
+assert(countPositionWithAim("dec2-mock.txt") == 900)
+print(countPositionWithAim("dec2.txt"))
