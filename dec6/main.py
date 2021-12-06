90e1d4f937d7a749b10facde52e42ab726947b76
Alexander Henry <10469240+afmhenry@users.noreply.github.com>
2021-12-06 23:14:51 +0100

    O(n) time! did it boys

--- dec6/main.py
+++ dec6/main.py
@@ -5,20 +5,48 @@ from threading import Thread
 
 def countFish(filename, days):
     fishies = []
-
     with open(filename, encoding="latin1") as f:
         for index, row in enumerate(csv.reader(f, delimiter=",")):
             fishies = list(row)
             fishies = [int(x) for x in fishies]
-            explode(fishies,days)
+            return explode(fishies, days)
+
+
+def countFishOptimized(filename, days):
+    fishies_optimized = [0 for i in range(9)]
+    with open(filename, encoding="latin1") as f:
+        for index, row in enumerate(csv.reader(f, delimiter=",")):
+            fishies = list(row)
+            fishies = [int(x) for x in fishies]
+            list.sort(fishies)
+            for i in range(len(fishies)):
+                fishies_optimized[fishies[i]] += 1
+    return explodeOptimized(fishies_optimized, days)
+
+
+def explodeOptimized(fishies, days):
+    shifted_fishies = []
+    shifted_fishies = [0 for k in range(9)]
+
+    for day in range(days):
+        for i in range(len(fishies)):
+            if i == 0:
+                shifted_fishies[8] = fishies[i]
+                shifted_fishies[6] = fishies[i]
+            elif 0 < i < 7:
+                shifted_fishies[i - 1] = fishies[i]
+            else:
+                shifted_fishies[i - 1] += fishies[i]
+
+        fishies = shifted_fishies
+        shifted_fishies = [0 for k in range(9)]
+
+    return sum(fishies)
 
 
 def explode(fishies, days):
-    days_unchanged = days
-    new_fishies = []
     for day in range(days):
-        print(day)
-
+        new_fishies = []
         for i in range(len(fishies)):
             fish_days = fishies[i] - 1
             if fish_days < 0:
@@ -30,5 +58,9 @@ def explode(fishies, days):
     return len(fishies)
 
 
-assert (countFish("dec6-mock.txt", 80) == 5934)
-print(countFish("dec6.txt", 80))
+assert (countFish("dec6-mock.txt", 18) == 26)
+print(countFish("dec6.txt", 80))  # 388739
+assert (countFishOptimized("dec6-mock.txt", 18) == 26)
+assert (countFishOptimized("dec6.txt", 80) == 388739)
+assert (countFishOptimized("dec6-mock.txt", 256) == 26984457539)
+print(countFishOptimized("dec6.txt", 256))  # 1741362314973
